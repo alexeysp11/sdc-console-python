eab5c9dd7b3db2090e898ef8d73d28b3717c61c5
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-08-26 13:05:58 +0500

    Use invoke

--- console/console.py
+++ console/console.py
@@ -4,21 +4,10 @@ When this app starts, user can enter some commands (for gps,
 gyro, fuzzy controller, computer vision etc). 
 When user writes some command, the program asks him or her 
 if it's okay to use default parameters.
-
-NOTES: 
-- Divide commands into submodules (sdc gps -p, sdc gps -v, sdc gps -a; 
-sdc gyro -s, sdc gyro -r). 
 """
 
-import sys
-sys.path.append('../RTLS')
-sys.path.append('../DATMO')
 import info
-import input_data
-from sensors.gps import GpsKF
-from sensors.gyro import GyroKF
-from lidar import Lidar
-
+import invoke
 
 if __name__ == '__main__':
     print('Welcome to SDC console!')
@@ -26,78 +15,41 @@ if __name__ == '__main__':
     while True:
         command = input('>> ')
         
-
         if command == 'sdc -info':
             info.info()
         
-
         elif command == ('sdc -h' or 'sdc -help'):
             info.help()
         
-
         elif command == 'sdc gps':
             info.gps()
 
-
         elif command == 'sdc gps --v':
-            velocity = 2.0
-
-            kf_1d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=1, init_velocity=velocity)
-            kf_1d.callkf(dimension=1, init_data=init_data)
-            
-            kf_2d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=2, init_velocity=velocity)
-            kf_2d.callkf(dimension=2, init_data=init_data)
+            invoke.gps(mode='v')
         
-
         elif command == 'sdc gps --p':
-            velocity = 0.0
-
-            kf_1d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=1, init_velocity=velocity)
-            kf_1d.callkf(dimension=1, init_data=init_data)
-            
-            kf_2d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=2, init_velocity=velocity)
-            kf_2d.callkf(dimension=2, init_data=init_data)
+            invoke.gps(mode='p')
         
-
         elif command == 'sdc gps --a':
-            velocity = 2.5
-
-            kf_1d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=1, init_velocity=velocity, accel=True)
-            kf_1d.callkf(dimension=1, init_data=init_data)
-            
-            kf_2d = GpsKF()
-            init_data = input_data.custom_or_default_data(dimension=2, init_velocity=velocity, accel=True)
-            kf_2d.callkf(dimension=2, init_data=init_data)
+            invoke.gps(mode='a')
         
-
         elif command == 'sdc gyro':
             info.gyro()
 
-
         elif command == 'sdc gyro --c':
-            gyro_1d = GyroKF()
-            gyro_1d.callkf(dim=2)
+            invoke.gyro(mode='c')
         
-
         elif command == 'sdc gyro --nu':
-            gyro_1d = GyroKF()
-            gyro_1d.callkf(dim=2, const_rotation=False)
-
+            invoke.gyro(mode='nu')
 
         elif command == 'sdc lidar':
-            lidar = Lidar()
-            lidar.call_lidar()
+            invoke.lidar()
         
-
         elif command == 'sdc nn mnist':
             pass
         
+        elif command == 'sdc fuzzy':
+            pass
 
         elif command == 'sdc exit':
             break
-        
--- console/invoke.py
+++ console/invoke.py
@@ -2,3 +2,46 @@
 Call IMU (GPS, gyro, accelerometer), DATMO (LIDAR, radar), 
 Neural Networks and Fuzzy controller.
 """
+
+import sys
+sys.path.append('../RTLS')
+sys.path.append('../DATMO')
+import input_data
+from sensors.gps import GpsKF
+from sensors.gyro import GyroKF
+from lidar import Lidar
+
+
+def gps(mode):
+    if mode == 'p':
+        velocity = 0.0
+        accel = False
+    elif mode == 'v':
+        velocity = 2.0
+        accel = False
+    elif mode == 'a':
+        velocity = 2.5
+        accel = True
+
+    kf_1d = GpsKF()
+    init_data = input_data.custom_or_default_data(dimension=1, init_velocity=velocity, accel=accel)
+    kf_1d.callkf(dimension=1, init_data=init_data)
+    
+    kf_2d = GpsKF()
+    init_data = input_data.custom_or_default_data(dimension=2, init_velocity=velocity, accel=accel)
+    kf_2d.callkf(dimension=2, init_data=init_data)
+
+
+def gyro(mode):
+    if mode == 'c':
+        const_rotation = True
+    elif mode == 'nu':
+        const_rotation = False
+
+    gyro_2d = GyroKF()
+    gyro_2d.callkf(dim=2, const_rotation=const_rotation)
+
+
+def lidar():
+    lidar = Lidar()
+    lidar.call_lidar()
