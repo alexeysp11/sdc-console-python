19764612bad4280108a735402f02deae37f04b4d
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-09-14 23:15:37 +0500

    Exception handling for input string in GPS module

--- env/velocity.py
+++ env/velocity.py
@@ -11,6 +11,7 @@ import numpy as np
 class Velocity: 
     def initialize(dimension=1, init_velocity=0.0, mode='p', is_accel=False, is_mock=True):
         if dimension == 1: 
+            # if we use default values
             if is_mock == True:
                 init_pos = 45.0
                 init_guess = 55.0
@@ -22,22 +23,57 @@ class Velocity:
                 print(f'Velocity (m/s): {velocity}')
                 print(f'Time (sec): {time_sec}')
             
+            # if we enter our own values
             else:
                 print('Initial data'.upper())
-                init_pos = float(input('Truth value: '))
-                init_guess = float(input('Initial guess: '))
                 
+                # input truth value
+                while (True):
+                    try:
+                        init_pos = float(input('Truth value: '))
+                        if type(init_pos) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+                
+                # input initial guess
+                while (True):
+                    try:
+                        init_guess = float(input('Initial guess: '))
+                        if type(init_guess) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+                
+                # if the car doesn't stay (e.g. it moves)
                 if mode != 'p':
-                    velocity = float(input('Velocity (m/s): '))
+                    # input velocity
+                    while (True):
+                        try:
+                            velocity = float(input('Velocity (m/s): '))
+                            if type(velocity) == float: 
+                                break
+                        except Exception as e:
+                            print('Exception: '.upper(), e)
+                
+                # if the car stays (e.g. it doesn't moves)
                 else: 
                     velocity = 0.0
                     print(f'Velocity (m/s): {velocity}')
                 
-                time_sec = int(input('Time (sec): ')) + 1
-            
+                # input time initerval
+                while (True):
+                    try:
+                        time_sec = int(input('Time (sec): ')) + 1
+                        if type(time_sec) == int: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+
             truth_value = Velocity.count_position(init_pos, velocity, is_accel, time_sec)
         
         if dimension == 2: 
+            # if we use default values
             if is_mock == True:
                 init_pos = (163.0, 55.0)
                 init_guess = (152.0, 68.0)
@@ -50,23 +86,82 @@ class Velocity:
                 print(f'Velocity_y: {velocity[1]} m/s')
                 print(f'Time (sec): {time_sec}')
             
+            # if we enter our own values
             else:
                 print('Initial data'.upper())
-                truth_value_X = float(input('Truth value X: '))
-                truth_value_Y = float(input('Truth value Y: '))
-                init_guess_X = float(input('Initial guess X: '))
-                init_guess_Y = float(input('Initial guess Y: '))
+
+                # input truth value with respect to X axis
+                while (True):
+                    try:
+                        truth_value_X = float(input('Truth value X: '))
+                        if type(truth_value_X) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+                
+                # input truth value with respect to Y axis
+                while (True):
+                    try:
+                        truth_value_Y = float(input('Truth value Y: '))
+                        if type(truth_value_Y) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+                
+                # input init guess with respect to X axis
+                while (True):
+                    try:
+                        init_guess_X = float(input('Initial guess X: '))
+                        if type(init_guess_X) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+                
+                # input init guess with respect to Y axis
+                while (True):
+                    try:
+                        init_guess_Y = float(input('Initial guess Y: '))
+                        if type(init_guess_Y) == float: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
                 
                 velocity = [0.0, 0.0]
                 
+                # if the car doesn't stay (e.g. it moves)
                 if mode != 'p':
-                    velocity[0] = float(input('Velocity X (m/s): '))
-                    velocity[1] = float(input('Velocity Y (m/s): '))
+                    # input velocity with respect to X axis
+                    while (True):
+                        try:
+                            velocity[0] = float(input('Velocity X (m/s): '))
+                            if type(velocity[0]) == float: 
+                                break
+                        except Exception as e:
+                            print('Exception: '.upper(), e)
+                    
+                    # input velocity with respect to Y axis
+                    while (True):
+                        try:
+                            velocity[1] = float(input('Velocity Y (m/s): '))
+                            if type(velocity[1]) == float: 
+                                break
+                        except Exception as e:
+                            print('Exception: '.upper(), e)
+
+                # if the car stays (e.g. it doesn't moves), 
+                # then its velovity should be equal zero
                 else: 
                     print(f'Velocity (m/s): {velocity}')
                 
-                time_sec = int(input('Time (sec): '))
-                
+                # input time initerval
+                while (True):
+                    try:
+                        time_sec = int(input('Time (sec): '))
+                        if type(time_sec) == int: 
+                            break
+                    except Exception as e:
+                        print('Exception: '.upper(), e)
+
                 # convert initial data into arrays 
                 init_pos = (truth_value_X, truth_value_Y)
                 init_guess = (init_guess_X, init_guess_Y)
